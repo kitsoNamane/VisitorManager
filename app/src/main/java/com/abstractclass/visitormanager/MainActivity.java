af673b7620fc610c7f82b2412315f128746b07e4
kitsoNamane <kitsogideonnamane@gmail.com>
2020-02-09 00:26:48 +0200

    added homePage, OCRpage and CarInfoPage
    Signed-off-by: kitsoNamane <kitsogideonnamane@gmail.com>

--- app/src/main/java/com/abstractclass/visitormanager/MainActivity.java
+++ app/src/main/java/com/abstractclass/visitormanager/MainActivity.java
@@ -1,15 +1,19 @@
 package com.abstractclass.visitormanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
 
 
 public class MainActivity extends AppCompatActivity {
+    public static NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
     }
 }
