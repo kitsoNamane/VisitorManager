cad3c1ce84b413f9d9ac150ebe0624815c746200
kitsoNamane <kitsogideonnamane@gmail.com>
2020-02-09 02:10:42 +0200

    added CarInfo view
    Signed-off-by: kitsoNamane <kitsogideonnamane@gmail.com>

--- app/src/main/java/com/abstractclass/visitormanager/MainActivity.java
+++ app/src/main/java/com/abstractclass/visitormanager/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.navigation.Navigation;
 
 import android.os.Bundle;
 
+import com.google.android.material.appbar.MaterialToolbar;
+
 
 public class MainActivity extends AppCompatActivity {
     public static NavController navController;
@@ -15,5 +17,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        final MaterialToolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 }
