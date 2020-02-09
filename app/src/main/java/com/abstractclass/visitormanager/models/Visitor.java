899d13240cacf61b26d6d1696fae8b464b3656ef
kitsoNamane <kitsogideonnamane@gmail.com>
2020-02-09 04:19:05 +0200

    add visitor and person models

--- app/src/main/java/com/abstractclass/visitormanager/models/Visitor.java
+++ app/src/main/java/com/abstractclass/visitormanager/models/Visitor.java
@@ -1,37 +1,50 @@
 package com.abstractclass.visitormanager.models;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Embedded;
+
 import com.google.gson.annotations.SerializedName;
 
 import java.io.Serializable;
 
-public class Visitor implements Serializable {
-    @SerializedName("first_name")
-    private String firstName;
+public class Visitor extends Person implements Serializable {
+    @Embedded
+    @SerializedName("person")
+    public Person person;
 
-    @SerializedName("last_name")
-    private String lastName;
-
-    @SerializedName("national_id")
-    private String nationalId;
-
-    @SerializedName("passport_number")
-    private String passportNumber;
-
-    @SerializedName("sex")
-    private String sex;
-
-    @SerializedName("birth_date")
-    private String birthDate;
-
-    @SerializedName("nationality")
-    private String nationality;
-
+    @ColumnInfo(name = "purpose")
     @SerializedName("purpose")
     private String purpose;
 
+    @ColumnInfo(name = "time_in")
     @SerializedName("time_in")
     private int timeIn;
 
+    @ColumnInfo(name = "time_out")
     @SerializedName("time_out")
     private int timeOut;
+
+    public String getPurpose() {
+        return purpose;
+    }
+
+    public void setPurpose(String purpose) {
+        this.purpose = purpose;
+    }
+
+    public int getTimeIn() {
+        return timeIn;
+    }
+
+    public void setTimeIn(int timeIn) {
+        this.timeIn = timeIn;
+    }
+
+    public int getTimeOut() {
+        return timeOut;
+    }
+
+    public void setTimeOut(int timeOut) {
+        this.timeOut = timeOut;
+    }
 }
