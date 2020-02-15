c0655221ecb4283b194f02dae8a69288f74afe64
kitsoNamane <kitsogideonnamane@gmail.com>
2020-02-15 21:52:07 +0200

    can now take picture and save it on device

--- app/src/main/java/com/abstractclass/visitormanager/PhotoIdFragment.kt
+++ app/src/main/java/com/abstractclass/visitormanager/PhotoIdFragment.kt
@@ -6,15 +6,16 @@ import android.content.pm.PackageManager
 import android.graphics.Matrix
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.util.Size
 import android.view.*
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import android.widget.Toast
-import androidx.camera.core.CameraX
-import androidx.camera.core.Preview
-import androidx.camera.core.PreviewConfig
+import androidx.camera.core.*
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import java.io.File
 import java.util.concurrent.Executors
 
 // TODO: Rename parameter arguments, choose names that match
@@ -54,6 +55,48 @@ class PhotoIdFragment : Fragment() {
     private lateinit var viewFinder: TextureView
 
     private fun startCamera() {
+
+        // Add this before CameraX.bindToLifecycle
+
+        // Create configuration object for the image capture use case
+        val imageCaptureConfig = ImageCaptureConfig.Builder()
+                .apply {
+                    // We don't set a resolution for image capture; instead, we
+                    // select a capture mode which will infer the appropriate
+                    // resolution based on aspect ration and requested mode
+                    setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
+                }.build()
+
+        // Build the image capture use case and attach button click listener
+        val imageCapture = ImageCapture(imageCaptureConfig)
+        activity?.findViewById<ImageButton>(R.id.capture_button)?.setOnClickListener {
+            val file = File(activity?.externalMediaDirs?.first(),
+                    "${System.currentTimeMillis()}.jpg")
+
+            imageCapture.takePicture(file, executor,
+                    object : ImageCapture.OnImageSavedListener {
+                        override fun onError(
+                                imageCaptureError: ImageCapture.ImageCaptureError,
+                                message: String,
+                                exc: Throwable?
+                        ) {
+                            val msg = "Photo capture failed: $message"
+                            Log.e("CameraXApp", msg, exc)
+                            viewFinder.post {
+                                Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
+                        override fun onImageSaved(file: File) {
+                            val msg = "Photo capture succeeded: ${file.absolutePath}"
+                            Log.d("CameraXApp", msg)
+                            viewFinder.post {
+                                Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    })
+        }
+
         // TODO: Implement CameraX operations
 
         // Create configuration object for the viewfinder use case
@@ -81,7 +124,7 @@ class PhotoIdFragment : Fragment() {
         // If Android Studio complains about "this" being not a LifecycleOwner
         // try rebuilding the project or updating the appcompat dependency to
         // version 1.1.0 or higher.
-        CameraX.bindToLifecycle(this, preview)
+        CameraX.bindToLifecycle(this, preview, imageCapture)
     }
 
     private fun updateTransform() {
